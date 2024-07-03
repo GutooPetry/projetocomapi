de06a248b15ec4bd5e0db3f26f73e6974c48ba19
GutooPetry <gustavoprogrammer24@gmail.com>
2024-07-03 08:50:55 -0300

    Update api.py

--- api.py
+++ api.py
@@ -78,9 +78,9 @@ def consulta_pagamento():
     cursor.execute(sql)
     identificador = cursor.fetchall()[0][0]
     sdk = mercadopago.SDK("TEST-6091877502706152-062313-26c59193cd69d48f61ed140114f7c596-1849541513")
-    i = 0 
-    cancelar_pagamento = st.form_submit_button(f'Cancelar Pagamento', key=f'{i}')
-    i += 1
+    
+    cancelar_pagamento = st.form_submit_button(f'Cancelar Pagamento')
+    
     
     filters = {
         "sort": "date_created",
